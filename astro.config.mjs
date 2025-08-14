47d1d90feea0a39e5fda366ab15b77e6c0a85ed1
YoshiaKefasu <yosiakefas.id@gmail.com>
2025-08-14 23:35:58 +0700

    Fix Root Base

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://gorakudo.org',
-  base: '/GoRakuDo',
+  base: '/',
   output: 'static' // Penting untuk GitHub Pages
 });
