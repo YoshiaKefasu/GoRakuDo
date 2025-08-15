01866e3042c8871bf2899c6ee8015d0172147dcf
Rio Saputro <rio.ae23@gmail.com>
2025-08-15 18:20:08 +0700

    Adding VueJS, Tailwind, documents layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+import path from 'path';
+
 export default defineConfig({
   site: 'https://gorakudo.org',
   base: '/',
-  output: 'static' // Penting untuk GitHub Pages
+  output: 'static',
+  integrations: [vue()],
+  vite:{
+    resolve: {
+      alias: {
+        '@' : path.resolve('./src')
+      }
+    }
+  },
+
+  // Penting untuk GitHub Pages
+
 });
