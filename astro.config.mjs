3d33a6e975257617f9e4c1ec1ded75cabea9da0b
YoshiaKefasu <yosiakefas.id@gmail.com>
2025-08-15 16:03:23 +0700

    Vueセットアップ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import vue from '@astrojs/vue';
+
 export default defineConfig({
   site: 'https://gorakudo.org',
   base: '/',
-  output: 'static' // Penting untuk GitHub Pages
+
+  // Penting untuk GitHub Pages
+  output: 'static',
+
+  integrations: [vue()]
 });
